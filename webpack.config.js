199b46429e22836d6d2d8010f2422ff619ffa48f
Javier Ledezma <juls0593@gmail.com>
2021-04-07 20:07:08 -0500

    Add fonts as resource in our pojet

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,20 @@ module.exports = {
             {
                 test: /.png/,
                 type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimeType: 'application/font-woff',
+                        name: '[name].[ext]',
+                        outputPath: './assets/fonts/',
+                        publicPath: './assets/fonts/',
+                        esModule: false
+                    }
+                }
             }
         ]
     },
