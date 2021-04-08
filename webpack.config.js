375e52ed9a100fa4f740750bf330a6b7e4e01c94
Javier Ledezma <juls0593@gmail.com>
2021-04-07 20:11:50 -0500

    Add minizer for js and css

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractConfig = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -66,5 +68,12 @@ module.exports = {
                 }
             ]
         })
-    ]
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
+    }
 }
